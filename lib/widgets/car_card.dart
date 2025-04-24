48ff872290f7f0ee34428617c2af2976b053a662
omer-akkoca <omerakkoca1042@gmail.com>
2025-04-25 01:44:35 +0300

    car card widget

---
+++ lib/widgets/car_card.dart
@@ -0,0 +1,65 @@
+import 'package:car_rental/components/svg_asset.dart';
+import 'package:car_rental/constants/images.dart';
+import 'package:car_rental/models/car.dart';
+import 'package:flutter/material.dart';
+
+class CarCard extends StatelessWidget {
+  final Car car;
+  const CarCard({super.key, required this.car});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: const Color(0xfff3f3f3),
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 10,
+              spreadRadius: 5,
+            )
+          ],
+        ),
+        child: Column(
+          children: [
+            Image.asset(CImages.carImage, height: 120),
+            Text(
+              car.model,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Row(
+                      children: [
+                        const SvgAsset(path: CImages.gpsIcon, width: 16, height: 16),
+                        Text(" ${car.distance.toStringAsFixed(0)}km")
+                      ],
+                    ),
+                    const SizedBox(width: 10),
+                    Row(
+                      children: [
+                        const SvgAsset(path: CImages.pumpIcon, width: 16, height: 16),
+                        Text(" ${car.fuelCapacity.toStringAsFixed(0)}km")
+                      ],
+                    ),
+                  ],
+                ),
+                Text(
+                  "\$${car.pricePerHour.toStringAsFixed(2)}",
+                  style: const TextStyle(fontSize: 16),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
