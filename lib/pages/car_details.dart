c49a7eee6efc99403f13635baeffba04c5a27427
omer-akkoca <omerakkoca1042@gmail.com>
2025-04-29 13:49:36 +0300

    mapImage animation

--- lib/pages/car_details.dart
+++ lib/pages/car_details.dart
@@ -5,16 +5,48 @@ import 'package:car_rental/widgets/car_card.dart';
 import 'package:car_rental/widgets/more_card.dart';
 import 'package:flutter/material.dart';
 
-class CarDetailsPage extends StatelessWidget {
+class CarDetailsPage extends StatefulWidget {
   final Car car;
 
   const CarDetailsPage({super.key, required this.car});
+
+  @override
+  State<CarDetailsPage> createState() => _CarDetailsPageState();
+}
+
+class _CarDetailsPageState extends State<CarDetailsPage> with SingleTickerProviderStateMixin {
+
+  AnimationController? _controller;
+  Animation<double>? _animation;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+
+    _animation = Tween<double>(begin: 1.0, end: 1.5).animate(_controller!)
+    ..addListener(() {
+     setState(() {});
+    });
+
+    _controller!.forward();
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    _controller!.forward();
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.white,
         title: const Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -27,7 +59,7 @@ class CarDetailsPage extends StatelessWidget {
         children: [
           Padding(
             padding: const EdgeInsets.all(16),
-            child: CarCard(car: car),
+            child: CarCard(car: widget.car),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16),
@@ -62,7 +94,7 @@ class CarDetailsPage extends StatelessWidget {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => MapsDetailsPage(car: car),
+                          builder: (context) => MapsDetailsPage(car: widget.car),
                         ),
                       );
                     },
@@ -70,13 +102,17 @@ class CarDetailsPage extends StatelessWidget {
                       height: 175,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(20),
-                        image: const DecorationImage(
-                          image: AssetImage(CImages.mapsImage),
-                          fit: BoxFit.cover,
-                        ),
                         boxShadow: const [
                           BoxShadow(color: Colors.black12, blurRadius: 10, spreadRadius: 5)
                         ],
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Transform.scale(
+                          scale: _animation!.value,
+                          alignment: Alignment.center,
+                          child: Image.asset(CImages.mapsImage, fit: BoxFit.cover,),
+                        ),
                       ),
                     ),
                   ),
@@ -88,13 +124,13 @@ class CarDetailsPage extends StatelessWidget {
             padding: const EdgeInsets.only(left: 16, right: 16, top: 16),
             child: Column(
               children: [
-                MoreCard(car: car),
+                MoreCard(car: widget.car),
                 const SizedBox(height: 8),
-                MoreCard(car: car),
+                MoreCard(car: widget.car),
                 const SizedBox(height: 8),
-                MoreCard(car: car),
+                MoreCard(car: widget.car),
                 const SizedBox(height: 8),
-                MoreCard(car: car),
+                MoreCard(car: widget.car),
               ],
             ),
           )
