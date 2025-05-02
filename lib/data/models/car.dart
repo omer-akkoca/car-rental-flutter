c96186719fabae1e24b0657ddbcd7f9bd2dc0148
omer-akkoca <omerakkoca1042@gmail.com>
2025-05-03 00:35:02 +0300

    location property for car model

--- lib/data/models/car.dart
+++ lib/data/models/car.dart
@@ -5,6 +5,7 @@ class Car {
   final double fuelCapacity;
   final double pricePerHour;
   final String image;
+  final Location location;
 
   Car({
     required this.id,
@@ -13,6 +14,7 @@ class Car {
     required this.fuelCapacity,
     required this.pricePerHour,
     required this.image,
+    required this.location,
   });
 
   factory Car.fromMap(Map<String, dynamic> map, String id) {
@@ -23,6 +25,18 @@ class Car {
       fuelCapacity: map["fuelCapacity"].toDouble(),
       pricePerHour: map["pricePerHour"].toDouble(),
       image: map["image"],
+      location: Location.fromMap(map["location"]),
     );
+  }
+}
+
+class Location {
+  final double longitude;
+  final double latitude;
+
+  Location({required this.longitude, required this.latitude});
+
+  factory Location.fromMap(Map<String, dynamic> map) {
+    return Location(longitude: map["longitude"], latitude: map["latitude"]);
   }
 }
